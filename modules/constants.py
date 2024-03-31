d5f2d169815aa8cabdda7079aab3a20e15ee235d
chenghui03 <chenghuihu03.@gmail.com>
2024-03-31 14:13:45 +0800

    implement Myseq class for biology seq

---
+++ modules/constants.py
@@ -0,0 +1,40 @@
+import re
+from Bio.Align import substitution_matrices
+
+ALPHABET= {
+    "DNA":"ATGC",
+    "RNA":"AUGC",
+    "PROTEIN":"ACDEFGHIKLMNPQRSTVWY"
+}
+
+ERROR_ALPHABET_PATTERN = {
+    "DNA":re.compile(r'[^ATGCN]'),
+    "RNA":re.compile(r'[^AUGCN]'),
+    "PROTEIN":re.compile(r'[^ACDEFGHIKLMNPQRSTVWY]')
+}
+
+CODEN = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+BLOSUM62 = {}
+__blosum_62_matrix = substitution_matrices.load("BLOSUM62")
+for i, aa1 in enumerate(__blosum_62_matrix.alphabet):
+    for j, aa2 in enumerate(__blosum_62_matrix.alphabet):
+        score = __blosum_62_matrix[aa1, aa2]
+        BLOSUM62[(aa1+aa2)] = score
+        BLOSUM62[(aa2+aa1)] = score 
---
+++ modules/seq.py
@@ -0,0 +1,89 @@
+""" Chapter 4"""
+
+import setwd
+
+import re
+import functools
+
+from modules.constants import * 
+
+
+class MySeq:
+    def __init__(self,seq:str,seq_type:str="DNA") -> None:
+        self.seq = seq.upper()
+        self.seq_type = seq_type
+
+    def __len__(self) -> int:
+        return len(self.seq)
+    
+    def __getitem__(self,index) -> str:
+        return self.seq[index]
+    
+    def __getslice__(self,i,j) -> str:
+        return self.seq[i:j]
+    
+    def __str__(self) -> str:
+        return self.seq
+    
+    def __repr__(self) -> str:
+        return f"Sequence: {self.seq} type: {self.seq_type}"
+    
+    def get_biotype(self) -> str:
+        return self.seq_type
+    
+    def alphabet(self) -> str|None:
+        return ALPHABET.get(self.seq_type,None)
+    
+    def validate(self) -> bool:
+        '''raise error when can't pass validation'''
+        match_iter = re.finditer(
+            ERROR_ALPHABET_PATTERN[self.seq_type],self.seq)
+        
+        for error in match_iter:
+            raise ValueError(
+                "{} at {} index is unsupported in alphabet".format(
+                    error.group()[0],error.span()[0]))
+        return True
+    
+    @staticmethod
+    def check_type(supported_type: list[str]):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                self = args[0]
+                if self.seq_type not in supported_type:
+                    raise ValueError(f"The sequence type '{self.seq_type}' is not supported. Expected {','.join(supported_type)} sequence.")
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
+        
+    @check_type(["DNA"])
+    def transcribe(self) -> "MySeq":
+        """without reverse"""
+        return MySeq(self.seq.replace("T", "U"), "RNA")
+    
+    @check_type(["DNA"])
+    def reverse_comp(self) -> "MySeq":
+        comp_table = str.maketrans('ACGTacgtRYMKrymkVBHDvbhd',
+                                    'TGCAtgcaYRKMyrkmBVDHbvdh')
+        seq = self.seq.translate(comp_table)
+        return MySeq(seq[::-1],"DNA")
+
+    def __translate_codon(self, codon) -> str:
+        return CODEN.get(codon, 'X')
+    
+    @check_type(["RNA"])
+    def retro_transcribe(self) -> "MySeq":
+        """without reverse"""
+        return MySeq(self.seq.replace("U", "T"), "DNA")
+
+    @check_type(["DNA", "RNA"])
+    def translate(self, ini_pos:int=0) -> "MySeq":
+        if self.seq_type == "DNA" :
+            seq = ''.join((self.__translate_codon(self.seq[pos:pos+3]) \
+                            for pos in range(ini_pos, len(self.seq)-2, 3)))
+            return MySeq(seq,"protein")
+        
+        elif self.seq_type == "RNA" :
+            return self.retro_transcribe().translate()
+        
